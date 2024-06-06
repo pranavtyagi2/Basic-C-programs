cc55717735872c1632df8f22de804b091df4fcbc
pranavtyagi2 <tyagipranav022005@gmail.com>
2024-06-06 22:15:31 +0530

    Add files via upload

---
+++ temperature in centigrade and fahrenheit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float C,f;
+    printf("Enter the temperature in centigrade=");
+    scanf("%f",&C);
+    f=(9*C/5+32);
+    printf("Enter the temperature in Fahrenheit=%f",f);
+
+}
